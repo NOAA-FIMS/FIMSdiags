fd4a0025aa3aca7e5cb932c90f5e22cb1bc3c4c5
Meg Oshima <78562167+MOshima-PIFSC@users.noreply.github.com>
2025-08-08 20:38:48 +0000

    adding code to get parameter values from fit
    Co-authored-by: Ian Taylor <iantaylor-NOAA@users.noreply.github.com>
    Co-authored-by: Andrea-Havron-NOAA <Andrea-Havron-NOAA@users.noreply.github.com>

--- R/fims_likelihood.r
+++ R/fims_likelihood.r
@@ -9,20 +9,28 @@ run_fims_likelihood <- function(parameters, data, parameter_label = "BevertonHol
     vec <- values + init
   # report the values
   cli::cli_alert_info("parameter values being profiles over: {paste(vec, collapse = ', ')}")
-run_modified_fims <- function(new_value, parameters, data){
-    parameters_mod <- parameters |> 
-        update_parameters(modified_parameters = list(recruitment = 
-        # TODO: figure out how to specify a generic parameter
-        list(BevertonHoltRecruitment.log_rzero.value = new_value,
-             BevertonHoltRecruitment.log_rzero.estimation_type = "constant")
-             )
-             )
-    data_model <- FIMSFrame(data)
-    new_fit <- parameters_mod |>
-      initialize_fims(data = data_model) |>
-      fit_fims(optimize = TRUE)
-    return(new_fit)
-} 
+    run_modified_fims <- function(new_value, parameters, data){
+        parameters_mod <- parameters |> 
+            update_parameters(modified_parameters = list(recruitment = 
+            # TODO: figure out how to specify a generic parameter
+            list(BevertonHoltRecruitment.log_rzero.value = new_value,
+                BevertonHoltRecruitment.log_rzero.estimation_type = "constant")
+                )
+                )
+        data_model <- FIMSFrame(data)
+
+        #TODO: replace parameters input with fitted FIMS model and use code below 
+        # to get final values of all parameters as a starting point
+        #like_fit$fits[[1]]@obj$env$last.par.best
+        #how to get parameter values with names: FIMS:::get_parameter_names(like_fit$fits[[1]]@obj$env$last.par.best)
+        #data object will be avaialble later
+        #like_fit$fits[[1]]@obj$env$data or #like_fit$fits[[1]]@obj$data
+
+        new_fit <- parameters_mod |>
+        initialize_fims(data = data_model) |>
+        fit_fims(optimize = TRUE)
+        return(new_fit)
+    } 
 
     fits <- furrr::future_map(
     .x = vec, 
