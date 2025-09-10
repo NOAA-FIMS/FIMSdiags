1e3692236a99e887b7adaf95f528aac0fc4fb065
iantaylor-NOAA <ian.taylor@noaa.gov>
2025-09-10 14:46:57 -0700

    work on plot_likelihood to generalize grouping

--- R/plot_likelihood.r
+++ R/plot_likelihood.r
@@ -2,33 +2,42 @@
 
 #' @param like_fit List returned by the [fims_likelihood()] function with
 #' model fits for each step in a likelihood profile
-#' @param type vector of data type to plot likelihood
-#' @param fleet vector of fleet(s) to show in plot (TODO: decide on name or number or either as input)
-plot_likelihood <- function(like_fit, type = NULL, fleet = NULL) {
-  # get column name for parameter being profiled
+#' @param group Character string or vector of strings with columns to group by, 
+#' e.g., "label" to group by data type, c("label", "fleet_name") to group by data 
+#' type and fleet (although fleet_name is not currently in FIMS output).
+plot_likelihood <- function(like_fit, group = "label") {
+
+ # get column name for parameter being profiled
   colname <- like_fit$fits |> names() |> grep(pattern = "value_", value = TRUE)
   if (length(colname) > 1) {
     cli::cli_abort("Function doesn't yet support multiple profile parameters")
   }
   if (length(colname) == 0) {
-    cli::cli_abort("No column with profile parameter found (column name should contain 'value_')")
+    cli::cli_abort(
+      "No column with profile parameter found (column name should contain 'value_')"
+    )
   }
-  
+  # TODO: add column to store the grouping variable(s)
+  # could use paste() to combine multiple grouping variables into one column
+  # then use that column for grouping below
   
   # summing total likelihood by parameter value, use later for plotting
   by_type <- like_fit$fits |>
-    dplyr::group_by(.data[[colname]], label) |>
+    dplyr::group_by(.data[[colname]], .data[[group]]) |>
     dplyr::summarise(total_like = -sum(log_like)) # negative to make negative log likelihood
-  total <- like_fit$fits |>
-    dplyr::group_by(.data[[colname]]) |>
-    dplyr::summarise(total_like = -sum(log_like)) |> # negative to make negative log likelihood
-    dplyr::mutate(label = "Total") |>
-    dplyr::select(.data[[colname]], label, total_like)
+
+  ### add total likelihood across all groups (TODO: turned off for now while we sort out new group column)
+  # total <- like_fit$fits |>
+  #   dplyr::group_by(.data[[colname]]) |>
+  #   dplyr::summarise(total_like = -sum(log_like)) |> # negative to make negative log likelihood
+  #   dplyr::mutate(label = "Total") |>
+  #   dplyr::select(.data[[colname]], label, total_like)
+
   # group the data type totals and the overall total and then
   # for each vector of sums, subtract the minimum across within that vector
-  grouped_like <- rbind(by_type, total) |>
+  grouped_like <- by_type |> # rbind(by_type, total) |>
     dplyr::arrange(.data[[colname]]) |>
-    dplyr::group_by(label) |>
+    dplyr::group_by(.data[[group]]) |>
     dplyr::mutate(total_like_change = total_like - min(total_like))
 
   # plot all the lines
@@ -37,8 +46,9 @@ plot_likelihood <- function(like_fit, type = NULL, fleet = NULL) {
     ggplot2::geom_line(ggplot2::aes(
       x = .data[[colname]],
       y = total_like_change,
-      color = label,
-      linewidth = ifelse(label == "Total", 2, 0.8)
+      color = .data[[group]] #,
+      # 
+      # linewidth = ifelse(.data[[group]] == "Total", 2, 0.8)
     )) +
     ggplot2::scale_linewidth_identity(guide = "none") +
     ggplot2::labs(
@@ -47,7 +57,6 @@ plot_likelihood <- function(like_fit, type = NULL, fleet = NULL) {
       color = "Data Type"
     )
   ggplot2::ggsave("likelihood.png")
-
 
   # improve x and y labels
   # make total line black
