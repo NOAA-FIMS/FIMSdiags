2c0756146322c89de3cf30481a1736b8b292dc9e
Meg Oshima <78562167+MOshima-PIFSC@users.noreply.github.com>
2025-09-25 00:22:04 +0000

    update likelihood functions to use new parameter tibble

--- R/run_modified_fims.r
+++ R/run_modified_fims.r
@@ -1,27 +1,64 @@
-run_modified_fims <- function(new_value, parameters, data) { #TODO: Move this outside the likelihood function?
-    parameters_mod <- parameters |>
-      update_parameters(
-        modified_parameters = list(
-          # TODO: figure out how to specify a generic parameter
-          recruitment = list(
-            BevertonHoltRecruitment.log_rzero.value = new_value,
-            BevertonHoltRecruitment.log_rzero.estimation_type = "constant"
-          )
-        )
-      )
-    data_model <- FIMSFrame(data)
+#' Modify a parameter input and run a FIMS model
+#'
+#' @param new_value The new value to be changed in the FIMS model.
+#' @param parameter_name The name of the parameter value (as listed in parameters$label) to be modified
+#' @param module_name
+#' @param parameters The tibble of input parameters for a FIMS model
+#' @param data A dataframe of input data for FIMS model
+#'
+#' @return FIMS model fitted to the new parameter input value
+#' @export
 
-    #TODO: replace parameters input with fitted FIMS model and use code below
-    # to get final values of all parameters as a starting point
-    #like_fit$fits[[1]]@obj$env$last.par.best
-    #how to get parameter values with names: FIMS:::get_parameter_names(like_fit$fits[[1]]@obj$env$last.par.best)
-    #data object will be available later
-    #like_fit$fits[[1]]@obj$env$data or #like_fit$fits[[1]]@obj$data
+run_modified_fims <- function(
+  new_value, 
+  parameter_name,
+  module_name = NULL,
+  parameters, #TODO: replace parameters and input with fitted FIMS model once tibbles are avaialble in FIMS fit object 
+  data) {
 
-    new_fit <- parameters_mod |>
-      initialize_fims(data = data_model) |>
-      fit_fims(optimize = TRUE)
+  # Need to load packages for each worker for furrr functions
+  require(FIMS, quietly = TRUE)
+  require(dplyr, quietly = TRUE)
+  require(tidyr, quietly = TRUE)
+  require(cli, quietly = TRUE)
 
-    clear()
-    return(new_fit)
+ # if parameters is nested, then unnest
+  if ("data" %in% names(parameters)) {
+    parameters <- parameters |> tidyr::unnest(cols = data)
   }
+
+  # find the parameter
+  if (!is.null(module_name)) {
+    parameter_row <- parameters |> 
+      dplyr::filter(.data$module_name == module_name & label == parameter_name)
+    if (nrow(parameter_row) == 0) {
+      cli::cli_abort("Parameter with module name {module_name} and label {label} not found in parameters object")
+    }
+  } else {
+    parameter_row <- parameters |> 
+      dplyr::filter(label == parameter_name)
+    if (nrow(parameter_row) == 0) {
+      cli::cli_abort("Parameter with label {parameter_name} not found in parameters object")
+    }
+    if (nrow(parameter_row) > 1) {
+      cli::cli_abort("Multiple parameters with label {parameter_name} found in parameters object, please specify module_name")
+    }
+  }
+
+  # Update value
+  parameter_row$value <- new_value 
+  parameters_mod <- parameters |> 
+    dplyr::rows_update(
+      parameter_row,
+      by = c("module_name", "label")
+    )
+
+  data_model <- FIMSFrame(data)
+
+  new_fit <- parameters_mod |>
+    initialize_fims(data = data_model) |>
+    fit_fims(optimize = TRUE)
+
+  clear()
+  return(new_fit)
+}
