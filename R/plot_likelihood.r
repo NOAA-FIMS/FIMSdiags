c938cc2287a519623cf121a6bf9c8465999d3e5c
MOshima-PIFSC <megumi.oshima@noaa.gov>
2025-12-03 12:59:37 -1000

    update plotting style of likelihood plot

--- R/plot_likelihood.r
+++ R/plot_likelihood.r
@@ -27,12 +27,12 @@ plot_likelihood <- function(like_fit, group = "label") {
   # could use paste() to combine multiple grouping variables into one column
   # then use that column for grouping below
   
-  # summing total likelihood by parameter value, use later for plotting
+  # summing total likelihood by parameter value and data type, use later for plotting
   by_type <- like_fit$estimates |>
     dplyr::filter(!is.na(lpdf)) |>
-    dplyr::group_by(.data[[colname]], .data[[group]]) |> # need to group by fleet, but it's not present in tibble at the moment
+    dplyr::group_by(.data[[colname]], .data[[group]])|> # grouping by parameter and data type
     dplyr::distinct(lpdf) |>
-    dplyr::summarise(total_like = sum(lpdf))  # TODO: make sure lpdf is the value we want to use
+    dplyr::summarise(total_like = sum(lpdf))  
 
   ### add total likelihood across all groups (TODO: turned off for now while we sort out new group column)
   total <- like_fit$estimates |>
@@ -44,34 +44,61 @@ plot_likelihood <- function(like_fit, group = "label") {
     dplyr::select(.data[[colname]], label, total_like)
 
   # group the data type totals and the overall total and then
-  # for each vector of sums, subtract the minimum across within that vector
+  # for each vector of sums, subtract the maximum across within that vector
   grouped_like <- by_type |>  
     dplyr::bind_rows(total) |>
     dplyr::arrange(.data[[colname]]) |>
     dplyr::group_by(.data[[group]]) |>
-    dplyr::mutate(total_like_change = total_like - min(total_like))
+    dplyr::mutate(total_like_change = max(total_like) - total_like) #based on convo with Andrea (12/3), the C++ code is positive log-like and doesn't look like reshape_json_fits code converts to NLL, so using max(total_like) instead of min()
 
+
+# Get all unique group values
+all_groups_clean <- unique(gsub("_expected", "", grouped_like[[group]]))
+other_groups_clean <- setdiff(all_groups_clean, "Total")
+
+# Create color vector: black for Total, default ggplot colors for others
+color_values <- c(
+  "Total" = "black",
+  setNames(
+    viridisLite::viridis(length(other_groups_clean), option = "mako", 
+                        begin = 0.35, end = 0.9),
+    other_groups_clean
+  )
+)
+
+linetype_values <- c(
+  "Total" = "solid", 
+  setNames(rep(c("dashed", "dotted", "dotdash", "longdash"), 
+              length.out = length(other_groups_clean)), 
+          other_groups_clean)
+)
   # plot all the lines
   p1 <- grouped_like |>
+    dplyr::mutate(
+    group_clean = gsub("_expected", "", .data[[group]])
+    ) |>
     ggplot2::ggplot() +
     ggplot2::geom_line(ggplot2::aes(
       x = .data[[colname]],
       y = total_like_change,
-      color = .data[[group]] #,
-      # 
-      # linewidth = ifelse(.data[[group]] == "Total", 2, 0.8)
-    )) +
-    ggplot2::scale_linewidth_identity(guide = "none") +
+      colour = group_clean, 
+      linetype = group_clean
+    ), linewidth = 1.2) +
+    stockplotr::theme_noaa(discrete = TRUE) +
+    ggplot2::scale_color_manual(
+    values = color_values,
+    name = "Data Type"  
+  ) +
+  ggplot2::scale_linetype_manual(
+    values = linetype_values,
+    name = "Data Type"  
+  ) +
     ggplot2::labs(
       x = colname |> gsub("value_", "", x = _), # remove "value_" from label
-      y = "Change in negative log-likelihood",
+      y = "Change in log-likelihood",
       color = "Data Type"
-    )
+    ) 
   ggplot2::ggsave("likelihood.png")
-
-  # improve x and y labels
-  # make total line black
-  # add different line types for data types
 
   return(p1)
 }
