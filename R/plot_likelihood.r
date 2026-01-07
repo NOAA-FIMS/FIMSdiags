f5316379839ebed7dd012ba4eba7aba3c0804b13
MOshima-PIFSC <megumi.oshima@noaa.gov>
2026-01-07 09:15:25 -1000

    getting rid of no visible binding warnings

--- R/plot_likelihood.r
+++ R/plot_likelihood.r
@@ -9,6 +9,8 @@
 #' @return ggplot of likelihood profile
 #' @export 
 #' 
+#' @importFrom rlang .data
+#' 
 #' @examples 
 #' \dontrun{
 #' like_fit <- run_fims_likelihood(
@@ -23,11 +25,12 @@
 #'plot_likelihood(like_fit)
 #' }
 plot_likelihood <- function(like_fit, group = "label") {
- # check that like_fit is actually the output from fims_likelihood()
- if (!"vec" %in% names(like_fit) | !"estimates" %in% names(like_fit)) {
-  cli::cli_abort("like_fit needs to be a list returned by `fims_likelihood()` that contains `vec` and `estimates`")
- }
- # get column name for parameter being profiled
+  # check that like_fit is actually the output from fims_likelihood()
+  if (!"vec" %in% names(like_fit) | !"estimates" %in% names(like_fit)) {
+    cli::cli_abort("like_fit needs to be a list returned by `fims_likelihood()` that contains `vec` and `estimates`")
+  }
+  
+  # get column name for parameter being profiled
   colname <- like_fit$estimates |> names() |> grep(pattern = "value_", value = TRUE)
   if (length(colname) > 1) {
     cli::cli_abort("Function doesn't yet support multiple profile parameters")
@@ -37,25 +40,26 @@ plot_likelihood <- function(like_fit, group = "label") {
       "No column with profile parameter found (column name should contain 'value_')"
     )
   }
+  
   # TODO: add column to store the grouping variable(s)
   # could use paste() to combine multiple grouping variables into one column
   # then use that column for grouping below
   
   # summing total likelihood by parameter value and data type, use later for plotting
   by_type <- like_fit$estimates |>
-    dplyr::filter(!is.na(lpdf)) |>
-    dplyr::group_by(.data[[colname]], .data[[group]])|> # grouping by parameter and data type
-    dplyr::distinct(lpdf) |>
-    dplyr::summarise(total_like = sum(lpdf))  
+    dplyr::filter(!is.na(.data$lpdf)) |>
+    dplyr::group_by(.data[[colname]], .data[[group]]) |> # grouping by parameter and data type
+    dplyr::distinct(.data$lpdf) |>
+    dplyr::summarise(total_like = sum(.data$lpdf), .groups = "drop")  
 
-  ### add total likelihood across all groups (TODO: turned off for now while we sort out new group column)
+  ### add total likelihood across all groups
   total <- like_fit$estimates |>
-    dplyr::filter(!is.na(lpdf)) |>
+    dplyr::filter(!is.na(.data$lpdf)) |>
     dplyr::group_by(.data[[colname]]) |>
-    dplyr::distinct(lpdf) |>
-    dplyr::summarise(total_like = sum(lpdf)) |> # negative to make negative log likelihood
+    dplyr::distinct(.data$lpdf) |>
+    dplyr::summarise(total_like = sum(.data$lpdf), .groups = "drop") |>
     dplyr::mutate(label = "Total") |>
-    dplyr::select(.data[[colname]], label, total_like)
+    dplyr::select(.data[[colname]], .data$label, .data$total_like)
 
   # group the data type totals and the overall total and then
   # for each vector of sums, subtract the maximum across within that vector
@@ -63,55 +67,57 @@ plot_likelihood <- function(like_fit, group = "label") {
     dplyr::bind_rows(total) |>
     dplyr::arrange(.data[[colname]]) |>
     dplyr::group_by(.data[[group]]) |>
-    dplyr::mutate(total_like_change = max(total_like) - total_like) #based on convo with Andrea (12/3), the C++ code is positive log-like and doesn't look like reshape_json_fits code converts to NLL, so using max(total_like) instead of min()
+    dplyr::mutate(total_like_change = max(.data$total_like) - .data$total_like) |>
+    dplyr::ungroup()
 
+  # Get all unique group values
+  all_groups_clean <- unique(gsub("_expected", "", grouped_like[[group]]))
+  other_groups_clean <- setdiff(all_groups_clean, "Total")
 
-# Get all unique group values
-all_groups_clean <- unique(gsub("_expected", "", grouped_like[[group]]))
-other_groups_clean <- setdiff(all_groups_clean, "Total")
-
-# Create color vector: black for Total, default ggplot colors for others
-color_values <- c(
-  "Total" = "black",
-  setNames(
-    viridisLite::viridis(length(other_groups_clean), option = "mako", 
-                        begin = 0.35, end = 0.9),
-    other_groups_clean
+  # Create color vector: black for Total, default ggplot colors for others
+  color_values <- c(
+    "Total" = "black",
+    stats::setNames(
+      viridisLite::viridis(length(other_groups_clean), option = "mako", 
+                          begin = 0.35, end = 0.9),
+      other_groups_clean
+    )
   )
-)
 
-linetype_values <- c(
-  "Total" = "solid", 
-  setNames(rep(c("dashed", "dotted", "dotdash", "longdash"), 
-              length.out = length(other_groups_clean)), 
-          other_groups_clean)
-)
+  linetype_values <- c(
+    "Total" = "solid", 
+    stats::setNames(rep(c("dashed", "dotted", "dotdash", "longdash"), 
+                length.out = length(other_groups_clean)), 
+            other_groups_clean)
+  )
+  
   # plot all the lines
   p1 <- grouped_like |>
     dplyr::mutate(
-    group_clean = gsub("_expected", "", .data[[group]])
+      group_clean = gsub("_expected", "", .data[[group]])
     ) |>
     ggplot2::ggplot() +
     ggplot2::geom_line(ggplot2::aes(
       x = .data[[colname]],
-      y = total_like_change,
-      colour = group_clean, 
-      linetype = group_clean
+      y = .data$total_like_change,
+      colour = .data$group_clean, 
+      linetype = .data$group_clean
     ), linewidth = 1.2) +
     stockplotr::theme_noaa(discrete = TRUE) +
     ggplot2::scale_color_manual(
-    values = color_values,
-    name = "Data Type"  
-  ) +
-  ggplot2::scale_linetype_manual(
-    values = linetype_values,
-    name = "Data Type"  
-  ) +
+      values = color_values,
+      name = "Data Type"  
+    ) +
+    ggplot2::scale_linetype_manual(
+      values = linetype_values,
+      name = "Data Type"  
+    ) +
     ggplot2::labs(
       x = colname |> gsub("value_", "", x = _), # remove "value_" from label
       y = "Change in log-likelihood",
       color = "Data Type"
     ) 
+  
   ggplot2::ggsave("likelihood.png")
 
   return(p1)
--- R/plot_retrospective.r
+++ R/plot_retrospective.r
@@ -6,30 +6,35 @@
 #' 
 #' @return ggplot of SSB and F for each retrospective peel
 #' @export 
+#' 
+#' @importFrom rlang .data
 plot_retrospective <- function(retro_fit, quantity = c("spawning_biomass", "mortality_F")) {
 
- # filter rows in estimates_df to get spawning_biomass and mortality_F
+  # filter rows in estimates_df to get spawning_biomass and mortality_F
   retro_df <- retro_fit[["estimates"]] |>
-  dplyr::filter(label %in% quantity) |> #right now mortality_F is by year/age, so ignoring it for simplicity
-  #TODO: some summarization to get total annual F values? 
-  dplyr::select(label, year_i, age_i, estimated, uncertainty, retro_year) |> 
-  dplyr::mutate(lower_CI = estimated - (1.96 * uncertainty),
-  upper_CI = estimated + (1.96 * uncertainty),
-  retro_year = factor(retro_year)) #assuming uncertainty is SE, TODO: check this assumption
+    dplyr::filter(.data$label %in% quantity) |> #right now mortality_F is by year/age, so ignoring it for simplicity
+    #TODO: some summarization to get total annual F values? 
+    dplyr::select(.data$label, .data$year_i, .data$age_i, 
+                  .data$estimated, .data$uncertainty, .data$retro_year) |> 
+    dplyr::mutate(lower_CI = .data$estimated - (1.96 * .data$uncertainty),
+                  upper_CI = .data$estimated + (1.96 * .data$uncertainty),
+                  retro_year = factor(.data$retro_year)) #assuming uncertainty is SE, TODO: check this assumption
 
-retro_plot <- ggplot2::ggplot(data = retro_df, ggplot2::aes(x = year_i)) +
-ggplot2::geom_ribbon(ggplot2::aes(ymin = lower_CI, ymax = upper_CI, fill = retro_year), alpha = .45) +   # should this be conditional?
-ggplot2::geom_line(ggplot2::aes(y = estimated, color = retro_year, linetype = retro_year)) +
-stockplotr::theme_noaa(discrete = TRUE) 
+  retro_plot <- ggplot2::ggplot(data = retro_df, ggplot2::aes(x = .data$year_i)) +
+    ggplot2::geom_ribbon(ggplot2::aes(ymin = .data$lower_CI, 
+                                      ymax = .data$upper_CI, 
+                                      fill = .data$retro_year), alpha = .45) +   # should this be conditional?
+    ggplot2::geom_line(ggplot2::aes(y = .data$estimated, 
+                                    color = .data$retro_year, 
+                                    linetype = .data$retro_year)) +
+    stockplotr::theme_noaa(discrete = TRUE) 
 
-if(length(unique(quantity)) > 1){
-  retro_plot <- retro_plot + 
-  ggplot2::facet_wrap(~label, scales = "free_y", ncol = 1)
-}
+  if(length(unique(quantity)) > 1){
+    retro_plot <- retro_plot + 
+      ggplot2::facet_wrap(~.data$label, scales = "free_y", ncol = 1)
+  }
 
   ggplot2::ggsave("retrospective.png")
 
   return(retro_plot)
-
-
 }
